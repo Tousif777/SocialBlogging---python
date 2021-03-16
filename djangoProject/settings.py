c5af54ff47d0aa2c18affd3035b3e06e370d19b4
Tousif777 <44972069+Tousif777@users.noreply.github.com>
2021-03-17 00:02:29 +0600

    trying to push

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 't6z8bx2h9m1rkik$hoe@^&$+b3_(fi#3%^&#xgk2f7qnyw7nhs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','socialposts.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
